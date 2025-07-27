b57d4e723ad06241f93a99c4d3ddbd3a2211a991
Martin Boos <mboos@outlook.com>
2025-07-27 18:05:27 +0200

    chore(django): allow all hosts

--- src/piki/settings.py
+++ src/piki/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-l-r!4v2p5v^h601cp7%dlmmm1y8-b@jqpy^&m_t_s&j=cx-z7b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
