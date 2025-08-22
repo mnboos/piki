5980b54acad488d8bf9870aba35b2e24c0914ec0
Martin Boos <mboos@outlook.com>
2025-08-22 13:41:15 +0200

    feat(rknn): implement object detection on NPU

--- src/core/utils/ai.py
+++ src/core/utils/ai.py
@@ -1,184 +1,251 @@
 import time
 from pathlib import Path
 
-from ai_edge_litert.interpreter import Interpreter
-import ai_edge_litert.interpreter as interpreter
 import traceback
 import numpy as np
+from rknnlite.api import RKNNLite
 
 from .shared import prob_threshold, worker_ready
 
-# model_file = "ssd_mobilenet_v1_0.75_depth_quantized_300x300_coco14_sync_2018_07_18.tflite"
-# model_file = "ssd-mobilenet-v2-tflite-100-int8-default-v1.tflite"
-# model_file = "ssd-mobilenet-v2-tflite-fpn-100-uint8-default-v1.tflite"
-model_file = "efficientdet-tflite-lite0-int8-v1.tflite"
+QUANTIZE_ON = True
 
-COCO_LABELS = {
-    1: "person",
-    2: "bicycle",
-    3: "car",
-    4: "motorcycle",
-    5: "airplane",
-    6: "bus",
-    7: "train",
-    8: "truck",
-    9: "boat",
-    10: "traffic light",
-    11: "fire hydrant",
-    13: "stop sign",
-    14: "parking meter",
-    15: "bench",
-    16: "bird",
-    17: "cat",
-    18: "dog",
-    19: "horse",
-    20: "sheep",
-    21: "cow",
-    22: "elephant",
-    23: "bear",
-    24: "zebra",
-    25: "giraffe",
-    27: "backpack",
-    28: "umbrella",
-    31: "handbag",
-    32: "tie",
-    33: "suitcase",
-    34: "frisbee",
-    35: "skis",
-    36: "snowboard",
-    37: "sports ball",
-    38: "kite",
-    39: "baseball bat",
-    40: "baseball glove",
-    41: "skateboard",
-    42: "surfboard",
-    43: "tennis racket",
-    44: "bottle",
-    46: "wine glass",
-    47: "cup",
-    48: "fork",
-    49: "knife",
-    50: "spoon",
-    51: "bowl",
-    52: "banana",
-    53: "apple",
-    54: "sandwich",
-    55: "orange",
-    56: "broccoli",
-    57: "carrot",
-    58: "hot dog",
-    59: "pizza",
-    60: "donut",
-    61: "cake",
-    62: "chair",
-    63: "couch",
-    64: "potted plant",
-    65: "bed",
-    67: "dining table",
-    70: "toilet",
-    72: "tv",
-    73: "laptop",
-    74: "mouse",
-    75: "remote",
-    76: "keyboard",
-    77: "cell phone",
-    78: "microwave",
-    79: "oven",
-    80: "toaster",
-    81: "sink",
-    82: "refrigerator",
-    84: "book",
-    85: "clock",
-    86: "vase",
-    87: "scissors",
-    88: "teddy bear",
-    89: "hair drier",
-    90: "toothbrush",
-}
+OBJ_THRESH = 0.25
+NMS_THRESH = 0.45
+IMG_SIZE = 640
+
+CLASSES = ("person", "bicycle", "car", "motorbike ", "aeroplane ", "bus ", "train", "truck ", "boat", "traffic light",
+           "fire hydrant", "stop sign ", "parking meter", "bench", "bird", "cat", "dog ", "horse ", "sheep", "cow", "elephant",
+           "bear", "zebra ", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite",
+           "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup", "fork", "knife ",
+           "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza ", "donut", "cake", "chair", "sofa",
+           "pottedplant", "bed", "diningtable", "toilet ", "tvmonitor", "laptop	", "mouse	", "remote ", "keyboard ", "cell phone", "microwave ",
+           "oven ", "toaster", "sink", "refrigerator ", "book", "clock", "vase", "scissors ", "teddy bear ", "hair drier", "toothbrush ")
+
+
+def xywh2xyxy(x):
+    # Convert [x, y, w, h] to [x1, y1, x2, y2]
+    y = np.copy(x)
+    y[:, 0] = x[:, 0] - x[:, 2] / 2  # top left x
+    y[:, 1] = x[:, 1] - x[:, 3] / 2  # top left y
+    y[:, 2] = x[:, 0] + x[:, 2] / 2  # bottom right x
+    y[:, 3] = x[:, 1] + x[:, 3] / 2  # bottom right y
+    return y
+
+
+def filter_boxes(boxes, box_confidences, box_class_probs):
+    """Filter boxes with box threshold. It's a bit different with origin yolov5 post process!
+
+    # Arguments
+        boxes: ndarray, boxes of objects.
+        box_confidences: ndarray, confidences of objects.
+        box_class_probs: ndarray, class_probs of objects.
+
+    # Returns
+        boxes: ndarray, filtered boxes.
+        classes: ndarray, classes for boxes.
+        scores: ndarray, scores for boxes.
+    """
+    boxes = boxes.reshape(-1, 4)
+    box_confidences = box_confidences.reshape(-1)
+    box_class_probs = box_class_probs.reshape(-1, box_class_probs.shape[-1])
+
+    _box_pos = np.where(box_confidences >= OBJ_THRESH)
+    boxes = boxes[_box_pos]
+    box_confidences = box_confidences[_box_pos]
+    box_class_probs = box_class_probs[_box_pos]
+
+    class_max_score = np.max(box_class_probs, axis=-1)
+    classes = np.argmax(box_class_probs, axis=-1)
+    _class_pos = np.where(class_max_score >= OBJ_THRESH)
+
+    boxes = boxes[_class_pos]
+    classes = classes[_class_pos]
+    scores = (class_max_score* box_confidences)[_class_pos]
+
+    return boxes, classes, scores
+
+
+def process(input, mask, anchors):
+
+    anchors = [anchors[i] for i in mask]
+    grid_h, grid_w = map(int, input.shape[0:2])
+
+    box_confidence = input[..., 4]
+    box_confidence = np.expand_dims(box_confidence, axis=-1)
+
+    box_class_probs = input[..., 5:]
+
+    box_xy = input[..., :2]*2 - 0.5
+
+    col = np.tile(np.arange(0, grid_w), grid_w).reshape(-1, grid_w)
+    row = np.tile(np.arange(0, grid_h).reshape(-1, 1), grid_h)
+    col = col.reshape(grid_h, grid_w, 1, 1).repeat(3, axis=-2)
+    row = row.reshape(grid_h, grid_w, 1, 1).repeat(3, axis=-2)
+    grid = np.concatenate((col, row), axis=-1)
+    box_xy += grid
+    box_xy *= int(IMG_SIZE/grid_h)
+
+    box_wh = pow(input[..., 2:4]*2, 2)
+    box_wh = box_wh * anchors
+
+    box = np.concatenate((box_xy, box_wh), axis=-1)
+
+    return box, box_confidence, box_class_probs
+
+
+def nms_boxes(boxes, scores):
+    """Suppress non-maximal boxes.
+
+    # Arguments
+        boxes: ndarray, boxes of objects.
+        scores: ndarray, scores of objects.
+
+    # Returns
+        keep: ndarray, index of effective boxes.
+    """
+    x = boxes[:, 0]
+    y = boxes[:, 1]
+    w = boxes[:, 2] - boxes[:, 0]
+    h = boxes[:, 3] - boxes[:, 1]
+
+    areas = w * h
+    order = scores.argsort()[::-1]
+
+    keep = []
+    while order.size > 0:
+        i = order[0]
+        keep.append(i)
+
+        xx1 = np.maximum(x[i], x[order[1:]])
+        yy1 = np.maximum(y[i], y[order[1:]])
+        xx2 = np.minimum(x[i] + w[i], x[order[1:]] + w[order[1:]])
+        yy2 = np.minimum(y[i] + h[i], y[order[1:]] + h[order[1:]])
+
+        w1 = np.maximum(0.0, xx2 - xx1 + 0.00001)
+        h1 = np.maximum(0.0, yy2 - yy1 + 0.00001)
+        inter = w1 * h1
+
+        ovr = inter / (areas[i] + areas[order[1:]] - inter)
+        inds = np.where(ovr <= NMS_THRESH)[0]
+        order = order[inds + 1]
+    keep = np.array(keep)
+    return keep
+
+
+def yolov5_post_process(input_data):
+    masks = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+    anchors = [[10, 13], [16, 30], [33, 23], [30, 61], [62, 45],
+               [59, 119], [116, 90], [156, 198], [373, 326]]
+
+    boxes, classes, scores = [], [], []
+    for input, mask in zip(input_data, masks):
+        b, c, s = process(input, mask, anchors)
+        b, c, s = filter_boxes(b, c, s)
+        boxes.append(b)
+        classes.append(c)
+        scores.append(s)
+
+    boxes = np.concatenate(boxes)
+    boxes = xywh2xyxy(boxes)
+    classes = np.concatenate(classes)
+    scores = np.concatenate(scores)
+
+    nboxes, nclasses, nscores = [], [], []
+    for c in set(classes):
+        inds = np.where(classes == c)
+        b = boxes[inds]
+        c = classes[inds]
+        s = scores[inds]
+
+        keep = nms_boxes(b, s)
+
+        nboxes.append(b[keep])
+        nclasses.append(c[keep])
+        nscores.append(s[keep])
+
+    if not nclasses and not nscores:
+        return None, None, None
+
+    boxes = np.concatenate(nboxes)
+    classes = np.concatenate(nclasses)
+    scores = np.concatenate(nscores)
+
+    return boxes, classes, scores
+
 try:
     print("Loading model...")
-    model_path = str(Path(__file__).parent / model_file)
-    is_quant = "quant" in model_file or "int8" in model_file
+    rknn = RKNNLite(verbose=True)
 
-    # armnn_delegate = interpreter.load_delegate(
-    #     library="/home/martin/Downloads/ArmNN-linux-x86_64/delegate/libarmnnDelegate.so",
-    #     options={
-    #         "backends": "GpuAcc,CpuAcc",  # Prioritize GPU, fallback to CPU
-    #         "logging-severity": "info",  # Optional: for verbose logging
-    #     },
-    # )
-    # print("Arm NN delegate loaded successfully.")
-    # delegates_list = [armnn_delegate]
-    #
-    # interpreter = Interpreter(
-    #     model_path=model_path, experimental_delegates=delegates_list
-    # )
+    # pre-process config
+    print('--> Config model')
+    #rknn.config(mean_values=[[0, 0, 0]], std_values=[[255, 255, 255]], target_platform='rk3566')
+    print('done')
 
-    interpreter = Interpreter(model_path=model_path)
+    model_file = Path(__file__).parent / "models" / "yolov5s_relu.rknn"
+    assert model_file.is_file()
 
-    interpreter.allocate_tensors()
-    input_details = interpreter.get_input_details()
-    inp_id = input_details[0]["index"]
-    output_details = interpreter.get_output_details()
+    # Load ONNX model
+    print('--> Loading model')
+    ret = rknn.load_rknn(model_file.absolute())
+    if ret != 0:
+        print('Load model failed!')
+        exit(ret)
+    print('done')
 
-    height = 320
-    width = 320
-
-    interpreter.resize_tensor_input(input_details[0]["index"], (1, height, width, 3))
-    interpreter.allocate_tensors()  # Must re-allocate tensors after resizing
-
-    out_id0 = output_details[0]["index"]
-    out_id1 = output_details[1]["index"]
-    out_id2 = output_details[2]["index"]
-    out_id3 = output_details[3]["index"]
+    # Init runtime environment
+    print('--> Init runtime environment')
+    ret = rknn.init_runtime()
+    if ret != 0:
+        print('Init runtime environment failed!')
+        exit(ret)
+    print('done')
 
     worker_ready.set()
 
     def detect_objects(image: np.ndarray) -> tuple[int, list]:
-        assert image.shape[0] == width and image.shape[1] == height, (
-            f"Image shape is {image.shape}, but expected ({width}, {height})"
+        assert image.shape[0] == IMG_SIZE and image.shape[1] == IMG_SIZE, (
+            f"Image shape is {image.shape}, but expected ({IMG_SIZE}, {IMG_SIZE})"
         )
 
+        # image = cv2.resize(image, (IMG_SIZE, IMG_SIZE))
         input_data = np.expand_dims(image, axis=0)
 
         t0 = time.perf_counter()
-        interpreter.set_tensor(inp_id, input_data)
-        interpreter.invoke()
+        outputs = rknn.inference(inputs=[input_data], data_format=['nhwc'])
         tt = round((time.perf_counter() - t0) * 1000)
 
-        boxes = interpreter.get_tensor(output_details[0]["index"])[0]
-        classes = interpreter.get_tensor(output_details[1]["index"])[0]
-        scores = interpreter.get_tensor(output_details[2]["index"])[0]
-        num_det = interpreter.get_tensor(output_details[3]["index"])[0]
+        # post process
+        input0_data = outputs[0]
+        input1_data = outputs[1]
+        input2_data = outputs[2]
 
-        num_objects_found = int(num_det)
+        input0_data = input0_data.reshape([3, -1] + list(input0_data.shape[-2:]))
+        input1_data = input1_data.reshape([3, -1] + list(input1_data.shape[-2:]))
+        input2_data = input2_data.reshape([3, -1] + list(input2_data.shape[-2:]))
+
+        input_data = list()
+        input_data.append(np.transpose(input0_data, (2, 3, 0, 1)))
+        input_data.append(np.transpose(input1_data, (2, 3, 0, 1)))
+        input_data.append(np.transpose(input2_data, (2, 3, 0, 1)))
+
+        boxes, classes, scores = yolov5_post_process(input_data)
 
         results = []
-        threshold = prob_threshold.value
-        for i in range(num_objects_found):
-            confidence = round(float(scores[i]), 3)
-            coco_class_id = int(classes[i])
-            if (
-                coco_class_id
-                and coco_class_id in COCO_LABELS
-                and confidence >= threshold
-            ):
-                # Get bounding box coordinates and scale them to the original image size
-                ymin, xmin, ymax, xmax = boxes[i]
-                ymin = max(0.0, ymin)
-                xmin = max(0.0, xmin)
-                ymax = min(1.0, ymax)
-                xmax = min(1.0, xmax)
+        for box, cls, score in zip(boxes, classes, scores):
+            label = CLASSES[cls]
 
-                x = int(xmin * width)
-                y = int(ymin * height)
-                w = int((xmax - xmin) * width)
-                h = int((ymax - ymin) * height)
+            ymin, xmin, ymax, xmax = box
+            ymin = max(0.0, ymin)
+            xmin = max(0.0, xmin)
+            ymax = min(1.0, ymax)
+            xmax = min(1.0, xmax)
 
-                # Get the object name from the label map
-                label = COCO_LABELS[coco_class_id]
-                # print("label: ", label, confidence)
-                results.append((label, confidence, (x, y, w, h)))
+            x = int(xmin * IMG_SIZE)
+            y = int(ymin * IMG_SIZE)
+            w = int((xmax - xmin) * IMG_SIZE)
+            h = int((ymax - ymin) * IMG_SIZE)
+
+            results.append((label, score, (x, y, w, h)))
 
         return tt, results
 except:
