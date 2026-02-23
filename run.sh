59c43da76af95376bd2d128b6ebba17aa3418b64
Martin Boos <mboos@outlook.com>
2026-02-23 17:02:44 +0100

    build(run): use hbm

--- run.sh
+++ run.sh
@@ -35,7 +35,7 @@ export ROS_LOG_DIR=/userdata/.roslog
 # automatically and rectifies internally regardless of this flag.
 # Set to True only if you provide an external calibration_file_path override.
 echo "[piki] Starting hobot_stereonet..."
-ros2 launch hobot_stereonet stereonet_model.launch.py \
+ros2 launch hobot_stereonet stereonet_model_v2.2.launch.py \
     mipi_image_width:=640 \
     mipi_image_height:=352 \
     mipi_lpwm_enable:=True \
